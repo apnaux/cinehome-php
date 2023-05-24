a05bfd6ebb05e04509c3d313cd2cc39ff7005b2a
Chris <62326766+apnaux@users.noreply.github.com>
2023-05-24 16:37:34 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/public/index.css" rel="stylesheet">
+    <title>cinehome: The Home Theatre Experience</title>
+</head>
+<body>
+    <p class="text-5xl font-bold bg-red-300 text-red-900 italic ">
+        This is a test
+    </p>
+</body>
+</html>
---
+++ serve.sh
@@ -0,0 +1,2 @@
+npx tailwindcss -i input.css -o ./public/index.css
+php -S localhost:8000
