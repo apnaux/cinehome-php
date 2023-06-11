4c0e1fabd0a7ddc254a8a79a88c1ccb0bbd45853
Mikadea <deasismichael13@gmail.com>
2023-06-11 16:18:46 +0800

    Modal installed and details inside modals are already done.

--- index.php
+++ index.php
@@ -7,6 +7,53 @@
     <link href="/public/index.css" rel="stylesheet">
     <link href="/public/fonts.css" rel="stylesheet">
     <title>cinehome: The Home Theatre Experience</title>
+    <style>
+        /* The Modal (background) */
+    .modal {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    padding-top: 10px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 95%; /* Full width */
+    height: 95%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    }
+
+    /* Modal Content */
+    .modal-content {
+    background-color: #000FFF;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #FFFFFF;
+    width: 50%;
+    height: 95%;
+    align-content: justify;
+    overflow:  auto;
+    }
+
+    /* The Close Button */
+    .close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;                                                                 
+    }
+
+    .img-modal
+    {
+        border: 2px solid #808080;
+    }
+    </style>
+
 </head>
 <body class="bg-fixed bg-cover" style="background-image:url(/public/Images/Image-Background.jpg)">
     <header class="flex flex-row justify-between items-center px-8 h-20
@@ -36,7 +83,7 @@
                             font-instrument opacity-0">
                     <p class="text-center align-bottom">
                         Lorem Ipsum dolor amet
-                        
+
                         <br>
                         <br>
 
@@ -138,7 +185,7 @@
              style="background-image: url('public/Images/Poster_Images/EerieMovie-Poster.jpg')">
                 <div class="h-full w-full group-hover:bg-gradient-to-b group-hover:opacity-100
                             from-transparent to-90% to-black p-6 text-white font-bold text-lg
-                            font-instrument opacity-0">
+                            font-instrument opacity-0 ">
                     <p class="text-center align-bottom">
                         Lorem Ipsum dolor amet
 
@@ -146,8 +193,43 @@
                         <br>
 
                         Date Watched: May 01, 2023
+
+                        <br>
+                        <br>
+
+                        <button class="border border-neutral-50 hover:border-gray-50" id="show_details">View Details</button>
+
+                        <div id="Details" class="modal">
+
+                            <div class="modal-content">
+                                <span class="close">&times;</span>
+
+                                <center>
+                                   <img class="img-modal" src="/public/Images/Poster_Images/EerieMovie-Poster.jpg"
+                                        height="288" width="192">
+                                </center>    
+
+                                <div class="ml-8 mt-6 grid grid-flow-row auto-rows-max gap-4">
+                                    <div>
+                                        <p class="font-solitus text-white">Title: Lorem Ipsum</p>
+                                    </div>
+                                    <div>
+                                        <p class="font-solitus text-white">Runtime: 00:00:00</p>
+                                    </div>
+                                    <div>
+                                        <p class="font-solitus text-white">Purchase Date: June 01, 2023</p>
+                                    </div>
+                                    <div>
+                                        <p class="font-solitus text-white">Completed Watch Date: June 01,2023</p>
+                                    </div>
+                                </div>
+            
+                            </div>
+                        </div>
+
                     </p>
                 </div>
+
         </div>
 
         <div class="h-72 w-48 bg-cover bg-center group" 
@@ -231,5 +313,32 @@
         </div>
     </div>
 
+<script>
+// Get the modal
+var modal = document.getElementById("Details");
+
+// Get the button that opens the modal
+var btn = document.getElementById("show_details");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
 </body>
 </html>
